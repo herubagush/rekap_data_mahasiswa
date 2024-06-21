90231112f07d1426400e36039080123f47c2f802
herubagush <65428329+herubagush@users.noreply.github.com>
2024-06-21 13:03:41 +0700

    Update 1_Data_per_Prodi.py

--- streamlit_app/pages/1_Data_per_Prodi.py
+++ streamlit_app/pages/1_Data_per_Prodi.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_icon=":school:",
 )
 
-st.logo("images/logo.png", link="https://github.com/herubagush/rekap_data_mahasiswa/blob/main/streamlit_app/")
+#st.logo("images/logo.png", link="https://github.com/herubagush/rekap_data_mahasiswa/blob/main/streamlit_app/")
 
 st.title("Data Sebaran Mahasiswa Institut Teknologi Yogyakarta (ITY) per Prodi")
 
